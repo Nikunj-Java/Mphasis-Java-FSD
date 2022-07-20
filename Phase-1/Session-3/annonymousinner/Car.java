13f16200a8f482d1b1673590a918f15927bb4fc2
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-20 10:59:22 +0530

    Maps,inner class

--- Phase-1/Session-3/annonymousinner/Car.java
+++ Phase-1/Session-3/annonymousinner/Car.java
@@ -2,6 +2,10 @@ package com.simplilearn.annonymousinner;
 
 public interface Car {
 	
+	// interface in java is abstarct type which is used to describe behaviour 
+	//it is blue print of class
+	//interface in java is used to achieve abstarction
+	
 	public void start();
 	public void stop();
 
