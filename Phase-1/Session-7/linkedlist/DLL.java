d48798d1fbc52e89122ae9282393c663fdce400b
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-26 12:11:04 +0530

    matrix multiplication,Range Query,Linked List,circular linked list,DLL

---
+++ Phase-1/Session-7/linkedlist/DLL.java
@@ -0,0 +1,85 @@
+package com.simplilearn.linkedlist;
+
+public class DLL {
+	Node head;
+
+	class Node {
+		int data;
+		Node prev;
+		Node next;
+
+		Node(int d) {
+			data = d;
+		}
+	}
+
+	// insert node at first position
+	public void push(int new_data) {
+		Node new_Node = new Node(new_data);
+		new_Node.next = head;
+		new_Node.prev = null;
+		if (head != null) //insert node at first position when list is not empty
+			head.prev = new_Node;
+		head = new_Node; // insert when the list is empty
+	}
+	//insert node after a perticular node
+	public void InsertAfter(Node prev_Node, int new_data) {
+		if (prev_Node == null) {
+			System.out.println("The given previous node cannot be NULL ");
+			return;
+		}
+		Node new_node = new Node(new_data);
+		new_node.next = prev_Node.next;
+		prev_Node.next = new_node;
+		new_node.prev = prev_Node;
+		if (new_node.next != null)
+			new_node.next.prev = new_node;
+	}
+	//add new node at the end of the list
+	void append(int new_data) {
+		Node new_node = new Node(new_data);
+		new_node.next = null;
+		
+		Node last = head;
+		
+		//if list is empty
+		if (head == null) {
+			new_node.prev = null;
+			head = new_node;
+			return;
+		}
+		while (last.next != null)
+			last = last.next;
+		last.next = new_node;
+		new_node.prev = last;
+	}
+
+	public void printlist(Node node) {
+		Node last = null;
+		System.out.println("Traversal in forward Direction");
+		while (node != null) {
+			System.out.print(node.data + " ");
+			last = node;
+			node = node.next;
+		}
+		System.out.println();
+		System.out.println("Traversal in reverse direction");
+		while (last != null) {
+			System.out.print(last.data + " ");
+			last = last.prev;
+		}
+	}
+
+	public static void main(String[] args) 
+    	{
+			DLL dll = new DLL();
+			dll.append(6); 
+			dll.push(7);//insert at head or  first position
+			dll.push(1);//insert at head or  first position
+			dll.append(4);//insert at last position
+			dll.InsertAfter(dll.head.next, 8);
+			System.out.println("Created DLL is: ");
+			
+			dll.printlist(dll.head);
+    	}
+}
