4e9df262d7c23b00948eb1dbfbbb7c33120f15a4
unknown <ganitniduniyanikunj10@gmail.com>
2022-08-01 12:15:12 +0530

    Phase-2 notes

---
+++ Phase-2/Session-1/MyFirstWebProject/src/main/java/com/simplilearn/com/FirstServlet.java
@@ -0,0 +1,24 @@
+package com.simplilearn.com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends  HttpServlet   {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException , IOException
+	{
+		
+		PrintWriter out= resp.getWriter();
+		out.print("Welcome"); //write out put in browser
+		
+		
+	}
+	 
+	
+}
