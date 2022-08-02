1ea1c15168bb31d4e3f144fe541a562d890683de
unknown <ganitniduniyanikunj10@gmail.com>
2022-08-02 12:02:51 +0530

    Servlet Interface & Generic Servlet,Filters,cookie,URL Rewritting

---
+++ Phase-2/Session-2/URLRewritting/src/main/java/com/simplilearn/demo/DashboardServlet.java
@@ -0,0 +1,44 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out= resp.getWriter();
+		
+		
+		String tokenFromUrl= req.getParameter("userid");
+		
+		if(tokenFromUrl!=null) {
+			out.print("User Exist "+ tokenFromUrl);
+		}
+		else {
+			out.print("User Not Exist");
+		}
+		
+
+		
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	
+	
+
+}
---
+++ Phase-2/Session-2/URLRewritting/src/main/java/com/simplilearn/demo/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 
+		
+		String  token= req.getParameter("userid");
+		
+		String  url="dashboard?userid="+token;
+		
+		resp.sendRedirect(url);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	
+	
+
+}
