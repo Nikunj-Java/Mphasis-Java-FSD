43c7c5bef5b801b4507a9db197f0efd8e5539b56
unknown <ganitniduniyanikunj10@gmail.com>
2022-08-08 11:51:34 +0530

    Dynamic webapp hibernate,annotations,log4j

---
+++ Phase-2/Session-6/HibernateWithAnnotationJava/src/com/simplilearn/demo/Student.java
@@ -0,0 +1,63 @@
+package com.simplilearn.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student_data")
+public class Student {
+	
+	@Id//primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//auto generation
+	@Column(name="sid")
+	private int id;
+	
+	@Column(name="sname")
+	private String name;
+	
+	private String email;
+	private String password;
+	
+	public Student() {
+		
+	}
+	public Student(String name, String email, String password) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+
+}
