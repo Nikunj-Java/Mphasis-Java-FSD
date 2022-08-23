677709fe450c0860471ad8e1ddb1215224ad786a
unknown <ganitniduniyanikunj10@gmail.com>
2022-08-23 10:58:54 +0530

    apache kafka,https

---
+++ Phase-3/Session-4/HttpsSpringBoot/src/main/java/com/simplilearn/demo/HttpsSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.simplilearn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.simplilearn.demo")
+public class HttpsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsSpringBootApplication.class, args);
+	}
+
+}
