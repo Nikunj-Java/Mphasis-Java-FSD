162b41ee93b71e28816efd5d0afcf8e8682c8d06
unknown <ganitniduniyanikunj10@gmail.com>
2022-09-13 12:20:12 +0530

    Arrow Function,Array operators,Rest and Default Parameters,Maps,classes,expense tracker,Angular

---
+++ Phase-4/Session-4/Angular/first.js
@@ -0,0 +1,10 @@
+var a;
+a = 56;
+var b = 12;
+var fname = "Nikunj Soni";
+console.log(a + b);
+console.log(fname);
+function test(num1, num2) {
+    return num1 + num2;
+}
+console.log("Addition: " + test(2, 3));
---
+++ Phase-4/Session-4/Angular/first.ts
@@ -0,0 +1,16 @@
+let a:number;
+a=56;
+
+let b:number=12;
+
+let fname:string="Nikunj Soni";
+
+console.log(a+b);
+console.log(fname);
+
+
+function test(num1:number,num2:number){
+    return num1+num2;
+}
+
+console.log("Addition: "+test(2,3));
