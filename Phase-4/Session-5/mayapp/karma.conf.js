03786c558c26d9bd1330f915af571f4ede9330aa
unknown <ganitniduniyanikunj10@gmail.com>
2022-09-16 09:27:27 +0530

    Angular Components

--- Phase-4/Session-5/mayapp/karma.conf.js
+++ Phase-4/Session-5/mayapp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/myapp'),
+      dir: require('path').join(__dirname, './coverage/mayapp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Phase-4/Session-5/mayapp/src/app/app.component.spec.ts
+++ Phase-4/Session-5/mayapp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myapp'`, () => {
+  it(`should have as title 'mayapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myapp');
+    expect(app.title).toEqual('mayapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mayapp app is running!');
   });
 });
