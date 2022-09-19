b1beac9006e3e3fc6c0c9ae59673fe1a2611a6f4
unknown <ganitniduniyanikunj10@gmail.com>
2022-09-19 10:07:20 +0530

    CustomForm Validations

---
+++ Phase-4/Session-6/form/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Phase-4/Session-6/form/src/app/register/register.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  //declare formgroup variables
+  registerForm: FormGroup;
+
+  //declare boolean variable to get form submitted  or not?
+  submitted:boolean=false;
+
+  //inject formbuilder dependency to create form with validators
+  constructor(private builder:FormBuilder) { }
+
+  //when component initiate we will write code here to build form with validators
+  ngOnInit(): void {
+
+    this.registerForm= this.builder.group(
+      {
+        firstName:["",Validators.required],
+        lastName:["",Validators.required],
+        email:["",[Validators.required,Validators.email]],
+        password:["",[Validators.required,Validators.minLength(8)]]
+      }
+    );
+  }
+
+
+  //when usewr will click on submit button - method will be called
+  OnSubmit(){
+    this.submitted=true;
+    if(this.registerForm.invalid)
+      return;
+    else
+      alert("Form Submitted Successfully");
+  }
+
+  //to access the  form control in a view to display the error
+  get f(){
+    return this.registerForm.controls;
+  }
+
+}
