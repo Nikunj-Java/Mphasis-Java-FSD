a5e9b6aff4db64d1b2af8538b8d064210d0543ef
unknown <ganitniduniyanikunj10@gmail.com>
2022-09-20 12:21:03 +0530

    Pipes Demo,Routing Mechanism

--- Phase-4/Session-7/myapp/src/app/app-routing.module.ts
+++ Phase-4/Session-7/myapp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PipesComponent } from './pipes/pipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //path to redirect
+  {"path":"home",component:HomeComponent},
+  {"path":"about",component:AboutComponent},
+  {"path":"contact",component:ContactComponent},
+  {"path":"pipe",component:PipesComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Phase-4/Session-7/myapp/src/app/app.module.ts
+++ Phase-4/Session-7/myapp/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomPipe } from './CustomPipe';
 import { PipesComponent } from './pipes/pipes.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PipesComponent,
-    CustomPipe
+    CustomPipe,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
