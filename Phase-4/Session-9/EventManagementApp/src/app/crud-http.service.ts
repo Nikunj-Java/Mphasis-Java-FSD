a98d7507b209d8c8c413b55284ec97119f5e2d29
unknown <ganitniduniyanikunj10@gmail.com>
2022-09-22 09:29:23 +0530

    Event Management App

---
+++ Phase-4/Session-9/EventManagementApp/src/app/crud-http.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudHttpService {
+
+  //url:string="db.json";
+
+  //inject DI in constructor
+  constructor() { }
+  
+  //prepare methods here
+}
